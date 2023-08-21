82b69542eb9ff532d1af8549b432c2875f5d25ba
dcheng289 <derekwcheng289@gmail.com>
2023-08-21 18:37:05 -0400

    update process-bags script for updated fasterlio drivers

--- process-raw-bags/process-bags.sh
+++ process-raw-bags/process-bags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-dataset_json='/derek_data/dataset/lidar-dataset-UCM-0323.json'
+dataset_json='UCM-0523.json'
 
-source /home/derek/Workspaces/catkin_ws/devel/setup.bash
+source /home/derek/Workspaces/backpack_ws/devel/setup.bash
 
 
 process_bag() {
@@ -9,20 +9,21 @@ process_bag() {
   echo "Raw Bag: $2"
   echo "Bag: $3"
   echo "Lidar: $4"
-  echo "Model Path: $5"
+  echo "OS Metadata: $5"
+  echo "Model Path: $6"
   echo "--------------------"
 
   # Run rosbag info command and extract desired information
-  info_output=$(rosbag info --yaml /derek_data/dataset/bags/$1/$2)
+  info_output=$(rosbag info --yaml /derek_data/ARBORES/$1/input/$2)
   duration=$(echo "$info_output" | awk '/duration:/{print $2}')
   echo $duration
 
   # Switch to correct branch and build
   roscd scan2shape_launch/script/
   if [ $4 == "OS0-128" ]; then
-    git checkout devel/orchard_os0-128
+    git checkout factor_graph_dbh_orchard_os0-128
   elif [ $4 == "OS1-64" ]; then
-    git checkout devel/orchard_os1-64
+    git checkout factor_graph_dbh_orchard
   fi
   catkin build
 
@@ -32,46 +33,52 @@ process_bag() {
   sleep 1
 f
   # Change directory and run infer_node.py in the background
-  cd /home/derek/Workspaces/sloam_ws/src/generic-sloam/scan2shape/script/ && python infer_node.py -d "$5" &
+  cd /home/derek/Workspaces/sloam_ws/src/generic-sloam/scan2shape/script/ && python infer_node.py -d "$6" &
   infer_node_pid=$!
   sleep 1
 
   if [ $4 == "OS0-128" ]; then
-  echo "128"
+
     # Run fasterlio
-    roslaunch scan2shape_launch run_flio_with_driver.launch replay:=true metadata:=/derek_data/dataset/UCM-OS0-128-SensorTower.json use_ouster128:=true &
+    roslaunch ouster_ros replay.launch metadata:=/derek_data/ARBORES/$1/metadata/$5 & 
+    replay_pid=$!
+
+    roslaunch faster_lio mapping_ouster128.launch & 
     fasterlio_pid=$!
     sleep 5
 
     # Record bag
-    rosbag record -O "/derek_data/dataset/bags/$1/$3" /Odometry /tf /tf_static /os_node/segmented_point_cloud_no_destagger /tree_cloud /ground_cloud __name:=record_bag &
+    rosbag record -O "/derek_data/ARBORES/$1/processed/$3" /Odometry /tf /tf_static /os_node/segmented_point_cloud_no_destagger /tree_cloud /tree_cloud_world /ground_cloud /cloud_registered_body /ublox/fix /ublox/fix_velocity __name:=record_bag &
     sleep 1
 
     # Finally play the bag
-    rosbag play "/derek_data/dataset/bags/$1/$2" /ouster/imu_packets:=/os_node/imu_packets /ouster/lidar_packets:=/os_node/lidar_packets  --start 0.1
+    rosbag play "/derek_data/ARBORES/$1/input/$2" /ouster/imu_packets:=/os_node/imu_packets /ouster/lidar_packets:=/os_node/lidar_packets  --start 0.1
     playbag_pid=$!
 
   
   elif [ $4 == "OS1-64" ]; then
     # Run fasterlio
-    roslaunch scan2shape_launch run_flio_with_driver.launch replay:=true &
+    roslaunch ouster_ros replay.launch & 
+    replay_pid=$!
+
+    roslaunch faster_lio mapping_ouster64.launch & 
     fasterlio_pid=$!
     sleep 5
 
-    # Capture the bag and run the required commands
-    rosbag record -O "/derek_data/dataset/bags/$1/$3" /Odometry /tf /tf_static /os_node/segmented_point_cloud_no_destagger /tree_cloud /ground_cloud __name:=record_bag &
+    # Record bag
+    rosbag record -O "/derek_data/ARBORES/$1/processed/$3" /Odometry /tf /tf_static /os_node/segmented_point_cloud_no_destagger /tree_cloud /tree_cloud_world /ground_cloud /cloud_registered_body /ublox/fix /ublox/fix_velocity __name:=record_bag &
     sleep 1
 
-    # # Finally play the bag
-    rosbag play "/derek_data/dataset/bags/$1/$2" --start 0.1
+    # Finally play the bag
+    rosbag play "/derek_data/ARBORES/$1/input/$2" --start 0.1 --topics /os_node/lidar_packets /os_node/imu_packets /os_node/metadata /ublox/fix /ublox/fix_velocity
     playbag_pid=$!
   
   fi
 
   # Sleep duraiton + 5
   # sleep $duration
-  echo "Killing processes $cloud_node_pid $infer_node_pid $fasterlio_pid $playbag_pid"
-  kill -INT $cloud_node_pid $infer_node_pid $fasterlio_pid $playbag_pid
+  echo "Killing processes $cloud_node_pid $infer_node_pid $fasterlio_pid $playbag_pid $replay_pid"
+  kill -INT $cloud_node_pid $infer_node_pid $fasterlio_pid $playbag_pid $replay_pid
   rosnode kill /record_bag
   sleep 10
 
@@ -95,12 +102,14 @@ while IFS= read -r line; do
     dataset_array[2]=$(echo "$line" | awk '{print $2}' | tr -d ',"')
   elif [[ "$line" =~ "lidar\":" ]]; then
     dataset_array[3]=$(echo "$line" | awk '{print $2}' | tr -d ',"')
-  elif [[ "$line" =~ "model\":" ]]; then
+  elif [[ "$line" =~ "metadata\":" ]]; then
     dataset_array[4]=$(echo "$line" | awk '{print $2}' | tr -d ',"')
+  elif [[ "$line" =~ "model\":" ]]; then
+    dataset_array[5]=$(echo "$line" | awk '{print $2}' | tr -d ',"')
   fi
 done < $dataset_json
 
 # Iterate through each dataset and print its contents
-for ((i = 0; i < ${#datasets[@]}; i+=5)); do
-  process_bag "${datasets[i]}" "${datasets[i+1]}" "${datasets[i+2]}" "${datasets[i+3]}" "${datasets[i+4]}"
+for ((i = 0; i < ${#datasets[@]}; i+=6)); do
+  process_bag "${datasets[i]}" "${datasets[i+1]}" "${datasets[i+2]}" "${datasets[i+3]}" "${datasets[i+4]}" "${datasets[i+5]}"
 done
